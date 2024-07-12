8c7367dc6f7fe8cc2363dedab50717bae6bd3bc6
jaytalla <jay.ray.talla06102000@gmail.com>
2024-07-12 18:35:38 +0800

    Added blood splatter

--- HitCollider.gd
+++ HitCollider.gd
@@ -7,15 +7,35 @@ var hitCollider : CollisionShape2D
 export(NodePath) var healthPath
 var healthManager : EnemyHealth
 
+export(NodePath) var animationPath
+var animPlayer : AnimationPlayer
+
+
+# Blood effect
+var bloodEffect = load("res://prefabs/FX/Zombie/BloodSplatter1.tscn")
+
+
 func _ready(): 
 	if hitCollisionPath != null:
 		hitCollider = get_node(hitCollisionPath)
 	if healthPath != null:
 		healthManager = get_node(healthPath)
+	if animationPath != null:
+		animPlayer = get_node(animationPath)
 
 
 func _on_HitCollider_area_entered(area):
 	if area.is_in_group("Bullet"):
+		var bloodInstance = bloodEffect.instance()
+		get_tree().current_scene.add_child(bloodInstance)
+		bloodInstance.global_position = area.global_position
+#		bloodInstance.rotation = global_position.angle_to_point(area.global_position)
+		if area.global_position.x > global_position.x:
+			bloodInstance.scale.x = 1
+		else:
+			bloodInstance.scale.x = -1
+		
+		animPlayer.play("hit_normal")
 		var bullet : RegularBullet = area
 		healthManager.DamageHealth(bullet.bulletDamage)
 		print("Damage taken " + str(bullet.bulletDamage))
